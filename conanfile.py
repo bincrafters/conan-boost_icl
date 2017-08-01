1136e5421f3887af8ee5428d06eff8829e9f5a7c
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:19:25 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,33 @@ class BoostIclConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/icl"
-    description = "For a description of this library, please visit http://boost.org/icl "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "icl"
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Container/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Date_Time/1.64.0@bincrafters/testing", \
+                      "Boost.Detail/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Move/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Rational/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
 
+                      #assert1 concept_check5 config0 container7 core2 date_time11 detail5 iterator5 move3 mpl5 rational6 static_assert1 type_traits3 utility5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
