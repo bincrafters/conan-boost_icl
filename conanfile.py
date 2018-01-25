a0dcd7e13e868a6723242dfa49791f9382ef7f41
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:42 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostIclConan(ConanFile):
     name = "boost_icl"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_icl"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["icl"]
     is_header_only = True
 
